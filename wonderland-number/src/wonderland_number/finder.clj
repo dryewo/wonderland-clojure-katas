78c0839db29018b0740415873d30623adff1e4a6
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2015-11-08 22:02:58 +0100

    Solve wonderland-number

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,19 @@
 (ns wonderland-number.finder)
 
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (first
+    (for [i (range 100000 (int (/ 999999 6)))
+          :when (->> (range 2 7)
+                     (sequence (comp
+                                 (map #(* i %))
+                                 (map str)
+                                 (map set)))
+                     (apply =))]
+      i)
+    #_(for [i (range 100000 (int (/ 999999 6)))
+            :when (->> (range 2 7)
+                       (map #(* i %))
+                       (map str)
+                       (map set)
+                       (apply =))]
+        i)))
