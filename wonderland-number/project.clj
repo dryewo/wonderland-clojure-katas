bb654b13175dd905fff4be22467c7a7465391fd2
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2015-11-08 21:59:54 +0100

    Enable reloaded workflow for wonderland-number

--- wonderland-number/project.clj
+++ wonderland-number/project.clj
@@ -3,5 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :main wonderland-number.finder)
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :main wonderland-number.finder
+  :profiles {:dev     {:source-paths ["dev"]
+                       :repl-options {:init-ns user}
+                       :dependencies [[org.clojure/tools.namespace "0.2.11"]
+                                      [org.clojure/java.classpath "0.2.2"]]}})
